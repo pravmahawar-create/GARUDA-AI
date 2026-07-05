92f7e795dbd5c0087a9e2c20898068022f4e586a
Praveen Mahawar <pravmahawar@gmail.com>
2026-07-05 22:56:57 +0530

    Add Mother Validator module

---
+++ scripts/mother/validator.js
@@ -0,0 +1,19 @@
+const { execSync } = require("child_process");
+
+function validate() {
+  console.log("[Validator] Starting...");
+
+  try {
+    execSync("node -c scripts/build-garuda.js", {
+      stdio: "inherit"
+    });
+
+    console.log("[Validator] Validation passed.");
+  } catch {
+    console.log("[Validator] Validation failed.");
+  }
+}
+
+module.exports = { validate };
+
+validate();
