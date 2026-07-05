705cea90fc50aec5a0a483f5c696ffe8422058ac
Praveen Mahawar <pravmahawar@gmail.com>
2026-07-05 22:53:19 +0530

    Add Mother Builder module

---
+++ scripts/mother/builder.js
@@ -0,0 +1,19 @@
+const { execSync } = require("child_process");
+
+function build() {
+  console.log("[Builder] Starting...");
+
+  try {
+    execSync("npm run build:garuda", {
+      stdio: "inherit"
+    });
+
+    console.log("[Builder] Build completed.");
+  } catch {
+    console.log("[Builder] Build failed.");
+  }
+}
+
+module.exports = { build };
+
+build();
