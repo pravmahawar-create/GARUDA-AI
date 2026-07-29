0fdcf967b9dd956224abb8bf2a7535cbb1386b48
Praveen Mahawar <pravmahawar@gmail.com>
2026-07-29 17:04:44 +0530

    fix: founder login navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import PublicLanding from "./pages/PublicLanding";
@@ -8,12 +8,20 @@ import FounderWorkspace from "./pages/FounderWorkspace";
 
 import "./styles/garuda-ui.css";
 
+function AppRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <Routes>
+      <Route path="/" element={<PublicLanding onLoginClick={() => navigate("/founder")} />} />
+      <Route path="/founder" element={<FounderWorkspace />} />
+      <Route path="*" element={<PublicLanding onLoginClick={() => navigate("/founder")} />} />
+    </Routes>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PublicLanding />} />
-      <Route path="/founder" element={<FounderWorkspace />} />
-      <Route path="*" element={<PublicLanding />} />
-    </Routes>
+    <AppRoutes />
   </BrowserRouter>
 );
