be9926fc069ffea531192efd9ef30a240147f317
Praveen Mahawar <pravmahawar@gmail.com>
2026-07-29 16:49:27 +0530

    fix: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import PublicLanding from "./pages/PublicLanding";
+import FounderWorkspace from "./pages/FounderWorkspace";
+
 import "./styles/garuda-ui.css";
 
-createRoot(document.getElementById("root")).render(<Home />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<PublicLanding />} />
+      <Route path="/founder" element={<FounderWorkspace />} />
+      <Route path="*" element={<PublicLanding />} />
+    </Routes>
+  </BrowserRouter>
+);
